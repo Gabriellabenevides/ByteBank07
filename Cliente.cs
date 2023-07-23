8ad9b2db94e53a2c37fcd47230b2997742f9f9f5
Gabriella Benevides <gabriellabenevides22@gmail.com>
2023-07-23 18:56:11 -0300

    first commit

---
+++ Cliente.cs
@@ -0,0 +1,20 @@
+﻿namespace ByteBank7
+{
+    public class Cliente
+    {
+        private string _cpf;
+        public string CPF 
+        { 
+            get
+            {
+                return _cpf;
+            }
+                set
+            { 
+                _cpf = value; 
+            }
+                }
+        public string Nome { get; set; }
+        public string Profissao { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ByteBank7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContaCorrente conta = new ContaCorrente(867, 86712540);
+
+        Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+
+
+        Console.WriteLine(conta.Agencia);
+        Console.WriteLine(conta.Numero);
+
+        ContaCorrente contaDaGabriela = new ContaCorrente(867, 86712540);
+        Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+        
+    }
+}
